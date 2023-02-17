4f30397f2c4d10a0a41ce135377a8fb6e62e5379
Rafael <bboyrafinhazika@gmail.com>
2023-02-17 18:33:18 -0300

    um dos scrapes mais dificeis

--- casasbahia_scrape.py
+++ casasbahia_scrape.py
@@ -1,12 +1,25 @@
 from bs4 import BeautifulSoup
-from response_handler import get_response
+from requests_html import HTMLSession
+import json
 
-def main():
-    url = 'https://tidd.ly/3RAzKeV'
-    response = get_response(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    price = soup.find('span', class_= 'product-price-value')
-    print(price.text)
+def scrape(url):
+    price, store = [-1, None]
+    session = HTMLSession(browser_args=["--no-sandbox", "--user-agent='Testing'"])
+    r = session.get(url)
+    r.html.render()
+    script = BeautifulSoup(r.html.raw_html, 'html.parser').find('script', {'type': 'application/json'})
+    try:
+        data = json.loads(script.contents[0])
+        sku = data['query']['sku']
+        url_data = f'https://pdp-api.casasbahia.com.br/api/v2/sku/{sku}/price/source/CB?utm_source=undefined&take=undefined&device_type=DESKTOP'
+        session2 = HTMLSession(browser_args=["--no-sandbox", "--user-agent='Testing'"])
+        r2 = session2.get(url_data)
+        r2.html.render()
+        json_data = json.loads(r2.html.html[r2.html.html.find('{'):r2.html.html.rfind('}')+1])
+        price, store = [json_data['paymentMethodDiscount']['sellPriceWithDiscount'], json_data['sellers'][0]['name']]
+    except Exception as e:
+        print(e)
+    print(price, store)
 
 if __name__ == '__main__':
-    main()
+    pass
