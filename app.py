f16e728d911f6820206e7b004bb7139d13ca0413
Rafael <bboyrafinhazika@gmail.com>
2023-02-21 15:50:55 -0300

    up

--- app.py
+++ app.py
@@ -47,7 +47,9 @@ def start(product):
 def main():
     products = api.get_products()
     browser = webdriver.Chrome(service=service, options=chrome_options)
-    concurrent.futures.ProcessPoolExecutor().map(start, products)
+    #concurrent.futures.ProcessPoolExecutor().map(start, products)
+    for product in products:
+        start(product)
 
 if __name__ == '__main__':
     main()
