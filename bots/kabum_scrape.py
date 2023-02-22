85683205920adf1e8c1dbfc94d7439c689ce9fa9
Rafael <bboyrafinhazika@gmail.com>
2023-02-21 22:48:03 -0300

    testing

--- bots/kabum_scrape.py
+++ bots/kabum_scrape.py
@@ -2,7 +2,6 @@ import re
 from bs4 import BeautifulSoup
 from api.scrape_request import get_response
 import json
-from requests_html import HTMLSession
 
 def coupon_validation(description, product):
     if description:
@@ -15,14 +14,11 @@ def coupon_validation(description, product):
     return True
 
 def scrape(url, params = None):
-    session = HTMLSession(browser_args=["--no-sandbox", "--user-agent='Testing'"])
-    r = session.get(url)
-    r.html.render()
-    site = BeautifulSoup(r.html.raw_html, 'html.parser')
-    print(site)
-    r.close()
-    session.close()
+    price = -1
+    store = None
+    response = get_response(url)
     try:
+        site = BeautifulSoup(response.content, 'html.parser')
         price = float(site.find('h4', class_=re.compile('finalPrice')).text[3:].replace('.', '').replace(',', '.'))
         store = site.find('div', class_=re.compile('generalInfo')).find('b').text
     except:
