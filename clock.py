2d4925ee81455c46c73f79ba24918b4c99f2a9b9
Rafael <bboyrafinhazika@gmail.com>
2023-02-22 14:19:30 -0300

    miss

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ import app
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', seconds=30)
 def timed_job():
     print('Starting scrape...')
     app.main()
