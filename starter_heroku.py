c031c8fc4d771fbfe0f8285bce0291f6eed957ee
Rafael <bboyrafinhazika@gmail.com>
2023-02-22 15:33:16 -0300

    final project

--- starter_heroku.py
+++
@@ -1,9 +0,0 @@
-from requests_html import HTMLSession
-import time
-import app
-def start():
-    HTMLSession(browser_args=["--no-sandbox"])
-    time.sleep(15)
-    app.main()
-if __name__ == '__main__':
-    start()
