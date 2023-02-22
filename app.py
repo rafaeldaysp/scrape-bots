83448e1a6a8e86306f0572a148874156fbd4da41
Rafael <bboyrafinhazika@gmail.com>
2023-02-22 14:19:12 -0300

    fix

--- app.py
+++ app.py
@@ -42,10 +42,9 @@ def start(product):
         print(product['title'], data)
 
 def main():
-    for i in range(5):
-        products = api.get_products()
-        #browser = webdriver.Chrome(service=service, options=chrome_options)
-        concurrent.futures.ProcessPoolExecutor().map(start, products)
+    products = api.get_products()
+    #browser = webdriver.Chrome(service=service, options=chrome_options)
+    concurrent.futures.ProcessPoolExecutor().map(start, products)
     # for product in products:
     #     start(product)
 
