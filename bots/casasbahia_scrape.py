22fca6e7cf1fb313df795a01922ff4901d0bb15b
Rafael <bboyrafinhazika@gmail.com>
2023-02-22 17:55:37 -0300

    fix

--- bots/casasbahia_scrape.py
+++ bots/casasbahia_scrape.py
@@ -14,32 +14,19 @@ def coupon_validation(description, product):
     return True
 
 def scrape(url, params = None):
-    price, store = [-1, None]
+    price, store = [-1, 'Casas Bahia']
     ua = str(UserAgent().chrome)
-    while True:
-        try:
-            session = HTMLSession(browser_args=["--no-sandbox", "--user-agent="+ua])
-            r = session.get(url)
-            r.html.render()
-            break
-        except Exception as e:
-            print('Scraping Casas Bahia bad request: ', e)
-            pass
-    script = BeautifulSoup(r.html.raw_html, 'html.parser').find('script', {'type': 'application/json'})
-    r.close()
-    session.close()
     try:
-        data = json.loads(script.contents[0])
-        sku = data['query']['sku']
-        url_data = f'https://pdp-api.casasbahia.com.br/api/v2/sku/{sku}/price/source/CB?utm_source=undefined&take=undefined&device_type=DESKTOP'
-        session2 = HTMLSession(browser_args=["--no-sandbox", "--user-agent='Testing'"])
-        r2 = session2.get(url_data)
-        r2.html.render()
-        json_data = json.loads(r2.html.html[r2.html.html.find('{'):r2.html.html.rfind('}')+1])
-        price, store = [json_data['paymentMethodDiscount']['sellPriceWithDiscount'], json_data['sellers'][0]['name']]
+        session = HTMLSession(browser_args=["--no-sandbox", "--user-agent="+ua])
+        r = session.get(url)
+        r.html.render(sleep=3)
+        site = BeautifulSoup(r.html.raw_html, 'html.parser')
+        price = site.find('span', class_='product-price-value').text[3:].replace('.', '').replace(',', '.')
     except Exception as e:
-        print(e)
+        print('Scraping Casas Bahia bad request: ', e)
+        print(r.html.html)
+        pass
     return price, store
 
 if __name__ == '__main__':
-    pass
+    scrape('https://www.casasbahia.com.br/notebook-lenovo-amd-ryzen-5-5500u-8gb-256gb-ssd-tela-full-hd-15-6-linux-ideapad-3-82mfs00100-55056427/p/55056427?')
