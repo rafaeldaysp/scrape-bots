e88d2bc00120cd74804b9adcf608ec9d9f6b0310
Rafael <bboyrafinhazika@gmail.com>
2023-03-01 23:10:55 -0300

    adding promos handlings

--- aliexpress_scrape.py
+++ aliexpress_scrape.py
@@ -31,10 +31,21 @@ def scrape(url, sku_id):
         try:
             slogan_banner = data['middleBannerModule']['uniformMiddleBanner']['sloganBanner']
             if slogan_banner == 'Preço exclusivo na primeira compra':
-                price_float_value += 16
+                price_float_value += 20
             ## colocar aqui quando tiver desconto de 15 a cada 150 
         except: 
             pass
+        
+        promo_desc = 0
+        try:
+            promo_15_off_a_cada = data['couponModule']['webCouponInfo']['promotionPanelDTO']['acrossStoreFixedDiscount'][0]['promotionPanelDetailDTOList'][0]['promotionDesc'].replace('(', '').replace(')', '')
+            values_promo = [int(s) for s in promo_15_off_a_cada.split() if s.isdigit()]
+            promo_desc = int(price_float_value/values_promo[1])*values_promo[0]
+            if promo_desc > values_promo[2]:
+                promo_desc = values_promo[2]
+        except:
+            pass
+        price_float_value -= promo_desc
         
         coupons = []
         coupons_conditions = []
@@ -105,4 +116,4 @@ def scrape(url, sku_id):
     return price, retalier
 
 if __name__ == '__main__':
-    print(scrape('https://pt.aliexpress.com/item/4000804563961.html?UTABTest=aliabtest376206_526413&aff_fcid=1d1a1c68511a45488f63cf8d298511f3-1677274789695-06841-_DcGLeg1&tt=CPS_NORMAL&aff_fsk=_DcGLeg1&aff_platform=shareComponent-detail&sk=_DcGLeg1&aff_trace_key=1d1a1c68511a45488f63cf8d298511f3-1677274789695-06841-_DcGLeg1&terminal_id=4ba0548134764b7786731ef60a8d595b&OLP=1084702908_f_group1&o_s_id=1084702908&afSmartRedirect=y', '12000031936107199'))
+    print(scrape('https://pt.aliexpress.com/item/1005005195329185.html?pdp_ext_f=%7B%22ship_from%22:%22CN%22,%22sku_id%22:%2212000032093838411%22%7D&&scm=1007.25281.317569.0&scm_id=1007.25281.317569.0&scm-url=1007.25281.317569.0&pvid=202bf4cf-e566-480d-8387-5057587255f5&utparam=%257B%2522process_id%2522%253A%252240%2522%252C%2522x_object_type%2522%253A%2522product%2522%252C%2522pvid%2522%253A%2522202bf4cf-e566-480d-8387-5057587255f5%2522%252C%2522belongs%2522%253A%255B%257B%2522floor_id%2522%253A%252236569244%2522%252C%2522id%2522%253A%252232032614%2522%252C%2522type%2522%253A%2522dataset%2522%257D%252C%257B%2522id_list%2522%253A%255B%255D%252C%2522type%2522%253A%2522gbrain%2522%257D%255D%252C%2522pageSize%2522%253A%252220%2522%252C%2522language%2522%253A%2522pt%2522%252C%2522scm%2522%253A%25221007.25281.317569.0%2522%252C%2522countryId%2522%253A%2522BR%2522%252C%2522scene%2522%253A%2522TopSelection-Waterfall%2522%252C%2522tpp_buckets%2522%253A%252221669%25230%2523265320%252341_21669%25234190%252319164%2523645_15281%25230%2523317569%25230%2522%252C%2522x_object_id%2522%253A%25221005005195329185%2522%257D&pdp_npi=2%40dis%21BRL%21R%24%20341%2C53%21R%24%20167%2C35%21%21%21%21%21%400b89a67f16777212742615583e0942%2112000032093838411%21gdf&spm=a2g0o.tm1000000741.4408202170.0&aecmd=true', '12000032093838411'))
